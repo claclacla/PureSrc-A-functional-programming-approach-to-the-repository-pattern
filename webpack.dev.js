d10dd75493018fd66d7ae6c3127415dfcc79a0b3
Simone Adelchino <seconddesire@gmail.com>
2018-05-05 10:20:57 +0200

    Created webpack development version

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,22 +1,24 @@
-const webpack = require('webpack');
 const path = require('path');
+const merge = require('webpack-merge');
+
+const common = require('./webpack.common.js');
 
 const SRC = path.resolve(__dirname, 'src');
 const E2E_TEST = path.resolve(__dirname, 'test/e2e');
 const PUBLIC = path.resolve(__dirname, 'public/js');
 
-var config = {
+var config = merge(common, {
   entry: E2E_TEST + '/index.js',
   output: {
     path: PUBLIC,
     filename: 'pureSrc.js'
   },
-  module : {
-    loaders : [
+  module: {
+    loaders: [
       {
-        test : /\.js?/,
-        include : E2E_TEST,
-        loader : 'babel-loader',
+        test: /\.js?/,
+        include: E2E_TEST,
+        loader: 'babel-loader',
         query: {
           plugins: ["babel-plugin-transform-object-rest-spread"]
         }
@@ -28,6 +30,6 @@ var config = {
       "@PureSrc": `${SRC}`,
     }
   }
-};
+});
 
 module.exports = config;
